621b6d24bb779839d04ad009ea580ab612b29b22
NguyenNgocLam <lamnn8@fpt.com.vn>
2020-02-16 13:06:44 +0700

    update

---
+++ session2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"strings"
+)
+
+func main(){
+	if len(os.Args) == 1 {
+		fmt.Printf("usage :%s <whole-number>\n",filepath.Base(os,Args[0]))
+		os.Exit(1)
+	}
+
+	stringOfDigits := os.Args[1]
+	for row := range bigDigits[0]{
+		line := ""
+		for column := range stringOfDigits {
+			digit := stringOfDigits[column]
+		}
+	}
+}
